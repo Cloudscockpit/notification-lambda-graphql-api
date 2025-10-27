b47288ca29a98e9a46f4cfe89f39b67b7ea3f5c5
TonmoyZohani <tonmoyzohani@gmail.com>
2025-10-27 14:21:03 +0600

    add

---
+++ src/types/notification.ts
@@ -0,0 +1,23 @@
+export interface Notification {
+  id: string;
+  message: string;
+  userId?: string | null;
+  boardId?: string | null;
+  createdAt: string;
+}
+
+export interface CreateNotificationInput {
+  message: string;
+  userId?: string;
+  boardId?: string;
+}
+
+export interface CreateForUserInput {
+  message: string;
+  userId: string;
+}
+
+export interface CreateForBoardInput {
+  message: string;
+  boardId: string;
+}
